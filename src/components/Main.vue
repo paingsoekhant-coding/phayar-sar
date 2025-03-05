687966183bc5974e4093e896e4132f6af532f7e4
Paing Soe Khant <babyboss7916@gmail.com>
2025-03-05 16:54:07 +0630

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container m-20">
+    <div class="">
+      <img src="../assets/favicon.png" alt="" class="w-40" />
+    </div>
+    <div class="">
+      <ul class="">
+        <li>heading one</li>
+        <li>heading two</li>
+        <li>heading three</li>
+        <li>heading four</li>
+        <li>heading five</li>
+        <li>heading six</li>
+        <li>heading seven</li>
+      </ul>
+    </div>
+  </div>
+</template>
