d4b9e0938670d1ef4e1e6af46d929f33a916dec5
Paing Soe Khant <babyboss7916@gmail.com>
2025-03-07 09:56:52 +0630

    Complete Detail UI

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,7 +1,12 @@
 <script setup>
 import Main from "@/components/Main.vue";
+import Detail from "@/components/Detail.vue";
+import Title from "@/components/Title.vue";
 </script>
 
 <template>
-  <Main></Main>
+  <div class="flex flex-row mt-18">
+    <Title></Title>
+    <Detail></Detail>
+  </div>
 </template>
